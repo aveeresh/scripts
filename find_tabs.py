928f78cebc603799f49ce63efc33e11f48458e58
aveeresh <veereshambe@gmail.com>
2025-10-29 18:47:16 +0530

    Added code to take care of exceptions during file processing

--- find_tabs.py
+++ find_tabs.py
@@ -6,11 +6,14 @@ for path, subdirs, files in os.walk(root):
 	for file in files:
 		if (file.endswith(".c")) or (file.endswith(".h")):
 			f_handle = open( os.path.join(path,file), "r" )
-			lines = f_handle.readlines()
-			
-			for line in lines:
-				if line.find("\t",0, len(line))!=-1:
-					print( os.path.join(path,file) )
-					break
+			try:
+				lines = f_handle.readlines()
+
+				for line in lines:
+					if line.find("\t",0, len(line))!=-1:
+						print( os.path.join(path,file) )
+						break
+			except:
+				print("Cannot process file - %s" % (file))
 
 
